7e34e04fa46f3f5bd1c17c2b1c9b828db369d585
Alex <exedis@yandex.ru>
2021-10-18 23:18:37 +0300

    add-func

--- src/features/FormAuth/FormAuth.jsx
+++ src/features/FormAuth/FormAuth.jsx
@@ -1,62 +1,97 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect,useCallback } from 'react';
 
 const FormAuth = () => {
-
     const [loadingServer, setLoadingServer] = useState(false);
     const [errors, setErrors] = useState('');
+    const [success, setSuccess] = useState('');
+    const [messages, setMessages] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [dataFromServer, setDataFromServer] = useState('');
+    const [authorized, setAuthorized] = useState(false);
 
-//     const checkAuth = () = {
-// return 1;
-//     }
-    if (localStorage.getItem('token')) {
-        console.log('token', localStorage.getItem('token'))
-    }
-
-    async function auth(e) {
-        e.preventDefault();
-        setLoadingServer(true);
+    //     const checkAuth = () = {
+    // return 1;
+    //     }
 
 
-        //запрашиваем данные из бд
+    function getServerInfo() {
         let data = {
             login: 'dev@dev.com',
             password: '123',
-            token: '132132asdasd',
-            authorized: false,
+            token: 'asdasda32423sdasdasda',
+            authorized: true,
         }
         data = JSON.stringify(data);
+        return new Promise(function (resolve, reject) {
 
-        let promise = new Promise((resolve, reject) => {
             setTimeout(() => {//имитируем задержку ответа
                 resolve(data);
-                setLoadingServer(false);
             }, 1000)
+
         });
+    }
 
-        let result = await promise.then((value => {
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            getServerInfo().then(
+                response => {
+                    const user = JSON.parse(response);
+                    if (user.authorized && user.token === token) {
+                        setAuthorized(true);
+                    }
+                }
+            )
+        }
+    }, [])
 
-            const valueFromServer = JSON.parse(value)
-            console.log('valueFromServer', valueFromServer)
-            if (valueFromServer.login === email && valueFromServer.password === password) {
-                localStorage.setItem('token', 'asdasda32423sdasdasda');
-                console.log('success')
-            } else {
-                console.log('failure');
-                setErrors('Ошибка! Неправильный логин или пароль!')
-                setTimeout(() => {
-                    setErrors('');
-                }, 2000)
-            }
 
-        }));
+    function login(e) {
 
-        return result;
+        e.preventDefault();
+        if (authorized) {
+            setMessages('Вы уже авторизованы!');
+            return;
+        }
+        setLoadingServer(true);
+
+        getServerInfo().then(
+            response => {
+                setLoadingServer(false);
+                const user = JSON.parse(response)
+
+                if (user.login === email && user.password === password) {
+                    localStorage.setItem('token', 'asdasda32423sdasdasda');
+                    setMessages('Успех! Добро пожаловать!');
+                } else {
+                    setMessages('Ошибка! Неправильный логин или пароль!')
+                }
+            },
+            error => { alert(`Rejected: ${error}`) }
+        );
+
 
     }
 
+    useEffect(() => {
+        const message = setTimeout(() => {
+            setMessages('');
+        }, 2000)
+        return () => message;
+    }, [messages, setMessages])
+
+    // useEffect(() => {
+    //    // localStorage.setItem('token', '');
+    // }, [messages, setMessages])
+
+ 
+    const logout = () => {
+        if (authorized) {
+            setMessages('Выход выполнен');
+            setAuthorized(false);
+            localStorage.setItem('token', '');
+        }
+    }
 
     const setEmailHandler = (val) => {
         setEmail(val);
@@ -67,24 +102,30 @@ const FormAuth = () => {
 
     return (
         <div>
+            {messages && <div>{messages}</div>}
             {loadingServer && <div>Загрузка</div>}
-            {errors && <div className={'error'}>{errors}</div>}
-            <form action="" onSubmit={(e) => auth(e)}>
+
+            <form action="" onSubmit={(e) => login(e)}>
                 <div className="formControl">
                     <div className="title">Выполнить вход</div>
                 </div>
                 <div className="formControl">
                     <input required type="email" placeholder={'Почта'} name={'email'} value={email}
-                           onChange={(e) => setEmailHandler(e.target.value)}/>
+                        onChange={(e) => setEmailHandler(e.target.value)} />
                 </div>
                 <div className="formControl">
                     <input required type="password" placeholder={'Пароль'} name={'pass'} value={password}
-                           onChange={(e) => setPasswordHandler(e.target.value)}/>
+                        onChange={(e) => setPasswordHandler(e.target.value)} />
                 </div>
                 <div className="formControl">
                     <button type={'submit'}>Войти</button>
                 </div>
+                {authorized && <div className={'authorized'}>
+                    <button onClick={logout} type={'button'}>Выйти</button>
+                </div>}
+
             </form>
+
         </div>
     );
 };
